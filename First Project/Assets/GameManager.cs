770f9d3673c6fb749a0c1909313a1ce36641f90e
Asaf Aviv <asafaviv89@gmail.com>
2019-06-28 12:45:37 +0300

    Added text animation and level transition

---
+++ First Project/Assets/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+    bool gameOver = false;
+
+    public float restartDelay = 1f;
+
+    public GameObject completeLevelUIl;
+    public void CompleteLevel() {
+        completeLevelUIl.SetActive(true);
+    }
+
+    public void EndGame() {
+        if (!gameOver) {
+            gameOver = true;
+            Debug.Log("Game Over");
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void Restart() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
