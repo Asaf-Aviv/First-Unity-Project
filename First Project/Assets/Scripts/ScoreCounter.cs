a0a91e1d8ac1962e8b044079f9544d742e61429b
Asaf Aviv <asafaviv89@gmail.com>
2019-06-28 04:01:41 +0300

    ScoreCounter

---
+++ First Project/Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCounter : MonoBehaviour {
+    public Transform player;
+    public Text scoreText;
+    void Update() {
+        scoreText.text = player.position.z.ToString("0");
+    }
+}
